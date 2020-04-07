42c50d192177bcfe051f9bd3c338455018c9fae2
Amit Krishna <23555312+amitkr936@users.noreply.github.com>
2020-04-07 20:33:04 +0530

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "DD       IIIIIIIII          RRRRRR" << std::endl;
+    std::cout << "DD D         II             RR    RRRR" << std::endl;
+    std::cout << "DD  DD       II             RR    RRR      EEEEE   NNNN   NN    GGGGGG    IIIIII " << std::endl;
+    std::cout << "DD   DDD     II             RRRRRRR        EE      NN NN  NN    GG          II"<< std::endl;
+    std::cout << "DD  DDD      II             RR    RR       EEEEE   NN  NN NN    GG  GGGG    II                "<< std::endl;
+    std::cout << "DD DD        II             RR      RR     EE      NN   NNNN    GG  GGGG    II               "<< std::endl;
+    std::cout << "DD       IIIIIIIII          RR        RR   EEEEE   NN    NN     GGGGGGGG  IIIIII  NE  ___" << std::endl;
     return 0;
 }
+
