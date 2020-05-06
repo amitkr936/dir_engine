e0bc2aed792c149b172eeee571e904ab6c2f3848
Amit Krishna <23555312+amitkr936@users.noreply.github.com>
2020-05-06 11:51:23 +0530

    chore(cpp):Minor changes in main and create Directory Object

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+
 //Declaring classes as these have inter-dependency
 class File;
 
@@ -33,18 +34,36 @@ public:
     vector<Directory *> child_dir;
     vector<File *> child_file;
 
-    Directory(Directory *parent, string name) {
-        this->parent = parent;
+    public: Directory(Directory ** parent, string name) {
+        this->parent = *parent;
         this->name = name;
     }
+
 };
 
+
+// a global pointer to point to the current directory
+
+Directory *current_dir;
 // A static Variable which will point to current directory
 
+
+void createdir()//method to create a
+{
+    string dir_name;
+    cout << "Enter Directory Name" << endl;
+    cin >> dir_name;
+    cout << dir_name << endl;
+    Directory* new_dir = new Directory(reinterpret_cast<Directory **>(current_dir), dir_name);
+    current_dir = new_dir;
+    //  Directory dir = new Directory();
+
+}
 int main() {
     //Creating root directory with parent as null
     string command;
     auto *root = new Directory(nullptr, "/");
+     current_dir = root;
     int choice;
    // <vector> string[] results;
     //TODO: Main function implementation
@@ -52,11 +71,12 @@ int main() {
         std::cout
                 << "Enter your Choice:\n 1:Create A directory \n 2: Create File \n 3:Move to upper Level of the tree \n 4: Move to Lower Level  \n 5:Traverse the tree"
                 << std::endl;
-        std::cin >> choice;
+        cin >> choice;
         //By default create Root Directory must be created
         switch (choice) {
             case 1:
-               // createdir();
+                //Directory.createdir();// for test case execution we will call a Function which will create an Object Instance
+                createdir();
                 //TODO: Create a Directory(Node Arbitrary Children)
                 break;
             case 2:
@@ -70,6 +90,9 @@ int main() {
                 break;
             case 5:
                 //TODO: Traverse the while tree(In-order traversal)
+                break;
+            case 6:
+                cout << current_dir->name << endl;
                 break;
             default:
                 //TODO: Handle otherwise
@@ -106,3 +129,4 @@ void removeDupWord(string str)
     }
     cout << word << endl;
 }*/
+
