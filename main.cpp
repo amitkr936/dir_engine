d1eb141a6897dd82c5a47e00ba1f6c1505ee4b6c
Sanket Bhat <sanket.bhat@sap.com>
2020-05-11 22:13:03 +0530

    Use linked list instead of vector

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream> //for std input/output
+#include <utility>
 #include <vector> //dynamically allocated list
 #include<string> // for string class
-#include<conio.h>
-#include<iterator>
 
 
 using namespace std;
 
-string CommandSplit[3];
 
 //Declaring classes as these have inter-dependency
 class File;
@@ -19,17 +17,26 @@ class File {
 public:
     string name;
     Directory *parent;
+    File *next;
+    File *prev;
 
     File(Directory *parent, string name) {
         this->parent = parent;
-        this->name = name;
+        this->name = std::move(name);
+        this->next = nullptr;
+        this->prev = nullptr;
     }
-    //TODO: Other properties of file
 
-    static void deleteFile(File* file)
-    {
-        file->parent = NULL;
-        free(file);
+    static void deleteFile(File *file) {
+        file->parent = nullptr;
+        file->prev = nullptr;
+        file->next = nullptr;
+
+        delete file;
+    }
+
+    static File *createFile(Directory *parent, string name) {
+        return new File(parent, std::move(name));
     }
 };
 
@@ -40,150 +47,112 @@ public:
     Directory *parent;
 
     //List of child directories and files
-    vector<Directory *> child_dir;
-    vector<File *> child_file;
+    Directory *child_dir;
+    Directory *next_dir;
+    Directory *pre_dir;
+
+    File **child_file;
 
     Directory(Directory *parent, string name) {
         this->parent = parent;
         this->name = name;
+        cout<<"Created dir with name"<<this->name<<endl;
+        this->child_dir = nullptr;
+        this->child_file = nullptr;
+        this->next_dir = nullptr;
+        this->pre_dir = nullptr;
     }
 
-    static void deleteDirectory(Directory* dir)//function to delete Directory
+    static void list(Directory *dir){
+        //TODO: Print all child dir and files
+        string child_name = ((dir->child_dir))->name;
+        cout<<child_name<<endl;
+    }
+
+    static void createDirectory(Directory *parent, Directory *newDir) {
+        //TODO: Insert into linked list
+        parent->child_dir = newDir;
+
+        cout<<"Creating dir under "<<parent->name<<" Directory name "<<((parent->child_dir))->name<<endl;
+    }
+
+    static void deleteDirectory(Directory *dir)//function to delete Directory
     {
-        if(dir->child_dir.size() == 0 && dir->child_file.size() == 0)
-        {
-            dir->parent = NULL;
-            free(dir);
-            return;
-        }
-
-        if(dir->child_dir.size() > 0)
-        {
-            for(vector<Directory*>::iterator i = dir->child_dir.begin(); i != dir->child_dir.end(); ++i)
-            {
-                deleteDirectory(*i);
-            }
-        }
-
-        if(dir->child_file.size() > 0)
-        {
-            for(vector<File*>::iterator i = dir->child_file.begin(); i != dir->child_file.end(); ++i)
-            {
-                File::deleteFile(*i);
-            }
-        }
+        //TODO:
+//        if (dir->child_dir.size() > 0) {
+//            for (vector<Directory *>::iterator i = dir->child_dir.begin(); i != dir->child_dir.end(); ++i) {
+//                deleteDirectory(*i);
+//            }
+//        }
+//
+//        if (dir->child_file.size() > 0) {
+//            for (vector<File *>::iterator i = dir->child_file.begin(); i != dir->child_file.end(); ++i) {
+//                File::deleteFile(*i);
+//            }
+//        }
+//        dir->parent = nullptr;
+//        delete dir;
     }
 };
-int setCommand(string ch)
-{
-    cout << ch << endl;
-    if(ch == "mkdir")
-    {
-        cout << "Mkdir" << endl;
-        //mkdir Function
-        return 1;
 
-    } else if (ch == "ls")
-    {
+Directory *root = new Directory(nullptr, "/");
+Directory *current_dir = root;
+
+int identifyCommand(string ch) {
+    string argument;
+    if (ch == "mkdir") {
+        cin >> argument;
+        auto *newDir = new Directory(current_dir, argument);
+        Directory::createDirectory(current_dir, newDir);
+
+        return 1;
+    } else if (ch == "ls") {
         cout << "list " << endl;
-        //call ls function()
-         return 0;
-    }
-    else if(ch == "cd")
-    {
-        cout << "Cd command" << endl;
+        Directory::list(current_dir);
         return 1;
-    } else
-    {
+    } else if (ch == "cd") {
+        cout << "Cd command" << endl;
+        cin >> argument;
+        return 1;
+    } else if (ch == "exit") {
+        return 0;
+    } else {
         cout << "<Please Enter Valid Command>" << endl;
-        return -1;
+        return 1;
     }
-
 }
+
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
 
 
 int main() {
 
-    Anfang://goto block in c++
     //Creating root directory with parent as null
     string command;
-    auto *root = new Directory(nullptr, "/");
-    int choice;
 
-    string Array[3];
-    /*
-     *  do {
-        std::cout
-                << "Enter your Choice:\n 1:Create A directory \n 2: Create File \n 3:Move to upper Level of the tree \n 4: Move to Lower Level  \n 5:Traverse the tree"
-                << std::endl;
-        std::cin >> choice;
-        //By default create Root Directory must be created
-        switch (choice) {
-            case 1:
-               // createdir();
-                //TODO: Create a Directory(Node Arbitrary Children)
-                break;
-            case 2:
-                //TODO: Create a File (Node without a Child)
-                break;
-            case 3:
-                //TODO: Move to Higher level Inside implement the method to traverse the current directory get all the child
-                break;
-            case 4:
-                //TODO: Move to Lower Level Inside implement the method to traverse the current directory get all the child
-                break;
-            case 5:
-                //TODO: Traverse the while tree(In-order traversal)
-                break;
-            default:
-                //TODO: Handle otherwise
-                exit(0);
-                break;
-        }
-    } while (true);*/
-    int ascii_value;
-    while(true)
-    {
-        char key_stroke;
-        static string ch="";
+    while (true) {
+        fflush(stdin);
 
-        /*key_stroke = getch();
-        ascii_value = key_stroke;
-        if(ascii_value == 72)
+        cout << "Enter the Command >";
+        cin >> command;
+        cout << command << endl;
+        int signal = identifyCommand(command);
+        if (signal == 0)//checks if the command is valid or not
         {
-            cout << "Upper key" << endl;
-        }*/
-        static int i;
-
-        while (getch() != '\n') {
-            i = 0;
-            cout << "Enter the Command ><$:::$" << endl;
-            cin >> ch;
-
-            cout << "Inp 1" << endl;
-            cout << ch << endl;
-            int signal  = setCommand(ch);
-            if(signal == -1)//checks if the command is valid or not
-            {
-                break;
-            }
-            if(signal == 0)
-            {
-                goto Anfang;// go to initial start block
-
-            }
-            i++;
-
-            if (ch.find(' ')) {
-                cin >> ch;
-                cout << "Inp 2" << endl;
-                cout << ch << endl;
-                //    Array[i++] = ch;
-                break;
-            }
+            break;
         }
     }
-
-
     return 0;
 }
+
+#pragma clang diagnostic pop
+
+/** ls
+ * cd <path />
+ *  cd ..
+ *  mkdir <dir-name>
+ *  rmdir <dir-name>
+ *  rm <file>
+ *  mkfile <file-name> / touch <file-name>*/
